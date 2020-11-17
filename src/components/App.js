87332bbc1cf74d3b3c1a40b6e49ade7de3cfe75e
Greg DeMaderios <demaderios@gmail.com>
2020-11-17 04:03:24 -0600

    Added VideoList and VideoItem along with styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 const KEY = process.env.REACT_APP_VIDEOS_API_KEY;
 
 class App extends Component {
-  onTermSubmit = (term) => {
-    console.log(term);
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        key: KEY,
+      },
+    });
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
