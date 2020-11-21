2a1842e6f29cbb3c464c718d11877449141b3b19
Greg DeMaderios <demaderios@gmail.com>
2020-11-21 16:23:01 -0600

    Added some accessibility code and formatted VideoDetail and VideoList into grid

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,7 +5,11 @@ import VideoList from "./VideoList";
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
     <div onClick={() => onVideoSelect(video)} className="video-item item">
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.title}
+      />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
       </div>
